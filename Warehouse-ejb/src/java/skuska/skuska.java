82e69a2d3056f1e280b58a12f45ac4c8bf116b7d
Gabo <Gabo@Gabo-PC>
2012-04-30 20:17:07 +0200

    Pridany Folder SourcePackages do ejb modulu

---
+++ Warehouse-ejb/src/java/skuska/skuska.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skuska;
+
+/**
+ *
+ * @author Gabo
+ */
+public class skuska {
+    
+}
