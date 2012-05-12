58b39af49ed262f47a500901170521c49c4bc289
Gabo <Gabo@Gabo-PC>
2012-05-12 16:16:16 +0200

    Pridany error checking pri vkladani master dat (ci sa nachadza uz v tabuľke)

--- Warehouse-ejb/src/java/topology/activeobject/MethodRequestInsertMD.java
+++ Warehouse-ejb/src/java/topology/activeobject/MethodRequestInsertMD.java
@@ -4,6 +4,8 @@
  */
 package topology.activeobject;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import persistence.Database;
 import persistence.MasterDataEntity;
 
@@ -15,6 +17,7 @@ public class MethodRequestInsertMD implements IMethodRequest {
 
     private Database database;
     private MasterDataEntity masterData;
+    private static final Logger LOGGER = Logger.getLogger(MethodRequestInsertMD.class.getName());
 
     public MethodRequestInsertMD(MasterDataEntity masterData, Database database) {
         this.masterData = masterData;
@@ -26,7 +29,11 @@ public class MethodRequestInsertMD implements IMethodRequest {
         Thread thread = new Thread(){
             @Override
             public void run(){
+                try{
                 database.addMasterData(masterData);
+                }catch(Exception e){
+                LOGGER.log(Level.INFO, "..............................Database Exception while inserting MD....................");
+                }
             }
         };
         thread.start();
